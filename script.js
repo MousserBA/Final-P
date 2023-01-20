010d14564be1548c9057d91ff950cf26ab24963b
MousserBA <luxus.immobiliere@gmail.com>
2023-01-20 10:22:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+count = 0;
+function counterFunc(){
+  count++;
+  document.getElementById("count").innerHTML = count;
+ }
+ function counterM(){
+  count--;
+  document.getElementById("count").innerHTML = count;
+ }
+function myFunction() {
+document.getElementById("click").innerHTML = "Réservé";
+}
